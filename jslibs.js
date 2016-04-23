f1af5ecae5aee7b7ba358f2cbaa9be230b5b6e0c
arubiopa <anggylotis@gmail.com>
2016-04-23 10:32:42 +0200

    ejercicio realizado

---
+++ jslibs.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+  $( "#datepicker" ).datepicker();
+  $( "#accordion" ).accordion();
+  $( "#accordion2" ).accordion();;
+  $( "#accordion3" ).accordion();;
+  $("#tabs").tabs();
+
+});
